a1687d21212be732e9f9fa69c6faabeba1e1e79d
jadenswang <jaden.shiteng.wang@gmail.com>
2018-01-15 21:02:37 -0500

    Update Tests and Quizzes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 function load(div) {
 
-    var x = ["oop", "enums", "FileInput", "FileOutput", "ResizeArrays", "AccessingObjects", "StringFormatting", "home", "about"];
+    var x = [
+        "oop", "enums", "FileInput", "FileOutput", "ResizeArrays", "AccessingObjects", "StringFormatting", 
+        "home", "about", 
+        "Quiz120417", "Quiz111517", "Test011718"];
 
-    for (var i = 0; i < x.length; i++) {        document.getElementById(x[i]).style.display = "none";
-
+    for (var i = 0; i < x.length; i++) {        
+        document.getElementById(x[i]).style.display = "none";
     }
 
     console.log(document.getElementById(div).style.display = "block");
